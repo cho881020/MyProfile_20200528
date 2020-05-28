00a5266b6a87dcb6a14d5691125b4d78f41b01c8
cho881020 <kj_cho@idealidea.co.kr>
2020-05-28 22:09:35 +0900

    닉네임 수정화면 추가 + 이동할때 startActivityForResult

---
+++ app/src/main/java/kr/co/tjoeun/myprofile_20200528/EditNickNameActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.myprofile_20200528
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNickNameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nick_name)
+    }
+}
--- app/src/main/java/kr/co/tjoeun/myprofile_20200528/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/myprofile_20200528/MainActivity.kt
@@ -1,12 +1,21 @@
 package kr.co.tjoeun.myprofile_20200528
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQ_FOR_NICKNAME = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        changeNickBtn.setOnClickListener {
+            val myIntent = Intent(this, EditNickNameActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_NICKNAME)
+        }
     }
 }
