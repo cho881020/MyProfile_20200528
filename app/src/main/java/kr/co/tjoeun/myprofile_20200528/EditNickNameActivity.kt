0f287641f0a6be28e48e1d2b1b197c7598e95d4a
cho881020 <kj_cho@idealidea.co.kr>
2020-05-28 22:13:26 +0900

    완료버튼을 누르면 => 결과를 세팅하고 기존 화면으로 복귀

--- app/src/main/java/kr/co/tjoeun/myprofile_20200528/EditNickNameActivity.kt
+++ app/src/main/java/kr/co/tjoeun/myprofile_20200528/EditNickNameActivity.kt
@@ -1,12 +1,28 @@
 package kr.co.tjoeun.myprofile_20200528
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+        okBtn.setOnClickListener {
+            val newNickName = nickNameEdt.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", newNickName)
+
+            setResult(Activity.RESULT_OK, resultIntent)
+
+            finish()
+
+        }
+
     }
 }
